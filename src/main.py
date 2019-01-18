95c77bdb91fe5ada9a82bedbfac094c8beba21e3
Christian Rattay <s6chratt@uni-bonn.de>
2019-01-18 01:54:54 +0100

    cleanup

--- src/main.py
+++ src/main.py
@@ -1,19 +1,13 @@
 from src.database_functions import *
 from src.commands import *
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
-from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters)
-from telegram import *
+from telegram.ext import (Updater, CommandHandler)
 
 
 def main():
-    """Start the bot."""
-
-    # ''' chris temp test bot '''
-    # updater = Updater("745861447:AAFgmej56K8weT-dpaxe97A6Ak-pTOptk-s")
 
     # SETUP Database
     setup_database()
-
 
     # Create the EventHandler and pass it your bot's token.
 #    bot = telegram.bot(token="702260882:AAF3VcoDbf3sSRVDht5xM3JYu-QNywEpgZg")
@@ -38,15 +32,8 @@ def main():
     dp.add_handler(CommandHandler("leeren", leeren))
     dp.add_handler(CommandHandler("help", help))
 
-    # on noncommand i.e message - echo the message on Telegram
-    # dp.add_handler(MessageHandler(Filters.text, echo))
-
     # Start the Bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
