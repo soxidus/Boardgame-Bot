e4fe3267196bb8fb6b2ad98ba35014ed29e94f24
karacolmar <k.moraw@uni-bonn.de>
2019-07-25 21:08:26 +0200

    Updating according to pep8

--- src/test.py
+++ src/test.py
@@ -1,10 +1,12 @@
 from database_functions import choose_database
 
+
 def main():
     db = choose_database('testdb')
     mycursor = db.cursor()
     household = 'a b'
-    mycursor.execute("INSERT INTO households (user_ids) VALUES (%s)", household)
+    mycursor.execute(
+        "INSERT INTO households (user_ids) VALUES (%s)", household)
     db.commit()
 
 
