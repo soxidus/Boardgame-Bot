182bfd9550ebc499bbf18b633c2e69ea7a6fe978
karacolmar <k.moraw@uni-bonn.de>
2019-08-23 22:48:08 +0200

    hardcode host

--- src/filters.py
+++ src/filters.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 from telegram.ext import BaseFilter
 from planning_functions import GameNight
 
--- src/planning_functions.py
+++ src/planning_functions.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 import datetime
 import re
 from random import randrange
