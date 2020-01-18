f4b43143ebab3d16e2b66e4a6e05ed572ccdd9cd
Christian R <s6chratt@uni-bonn.de>
2020-01-18 15:47:43 +0100

    parse date correct

--- src/calendar_export.py
+++ src/calendar_export.py
@@ -8,7 +8,7 @@ def create_ics_file(title, date):
     calendar = Calendar()
     gamenight_event = Event()
     gamenight_event.name = title
-    gamenight_event.begin = isoformat(date)
+    gamenight_event.begin = date.isoformat()
     calendar.events.add(gamenight_event)
     filename = 'cal-' + generate_uuid_32() + '.ics'
     GameNight(chat_id=update.callback_query.message.chat_id).set_cal_file(filename)
