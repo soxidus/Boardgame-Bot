3a658717c07f587f2409c0a0e1b0156fc9289618
Christian R <s6chratt@uni-bonn.de>
2020-01-18 01:19:01 +0100

    add actual ical functions

--- src/calendar_export.py
+++ src/calendar_export.py
@@ -1,13 +1,24 @@
 from ics import Calendar, Event
-from parse_strings import parse_date, generate_uuid_32
+from os import remove
+from datetime import isoformat
+from parse_strings import generate_uuid_32
 
 
 def create_ics_file(title, date):
     calendar = Calendar()
     gamenight_event = Event()
     gamenight_event.name = title
-    gamenight_event.begin = parse_date(date)
+    gamenight_event.begin = isoformat(date)
     calendar.events.add(gamenight_event)
-
-    with open(generate_uuid_32(), 'w+') as my_file:
+    filename = 'cal-' + generate_uuid_32() + '.ics'
+    GameNight(chat_id=update.callback_query.message.chat_id).set_cal_file(filename)
+    gamenight_event.make_all_day()
+    with open(filename, 'w+') as my_file:
         my_file.writelines(c)
+
+
+def delete_ics_file(filename):
+    try:
+        remove(filename)
+    except OSError:
+        pass
