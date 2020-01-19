0ca7a52a2279da4d9ad3476548ff51b203105cd8
Christian R <s6chratt@uni-bonn.de>
2020-01-19 21:27:58 +0100

    modify raspi-compose to init databases correctly

---
+++ infrastructure/init_data.sql
@@ -0,0 +1,8 @@
+USE testdb;
+CREATE TABLE games (title VARCHAR(255), owner VARCHAR(255), playercount VARCHAR(255), game_uuid VARCHAR(255), categories VARCHAR(255), last_played DATE);
+USE testdb;
+CREATE TABLE settings (user VARCHAR(255), notify_participation BIT(1) DEFAULT 1, notify_vote BIT(1) DEFAULT 1);
+USE testdb;
+CREATE TABLE expansions (title VARCHAR(255), owner VARCHAR(255), basegame_uuid VARCHAR(255));
+USE testdb;
+CREATE TABLE households (user_ids VARCHAR(255)); 
