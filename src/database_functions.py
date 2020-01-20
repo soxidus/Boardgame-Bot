164eb3a35bb5050dce3aa5e038a0104707616878
Christian R <s6chratt@uni-bonn.de>
2020-01-20 23:44:21 +0100

    add space to make query functional

--- src/database_functions.py
+++ src/database_functions.py
@@ -57,6 +57,7 @@ def add_game(db, table, entry, values):
 
     db.commit()
 
+
 # Searches all values from table by constraint
 def search_single_entry(db, table, entry, values, valuecnt=None):
     mycursor = db.cursor()
@@ -79,7 +80,7 @@ def search_single_entry(db, table, entry, values, valuecnt=None):
 
 def search_column_with_constraint(db, table, column, entry, values):
     mycursor = db.cursor()
-    sql = "SELECT" + column + "FROM " + table + " WHERE " + entry + " = " + str(values)
+    sql = "SELECT " + column + "FROM " + table + " WHERE " + entry + " = " + str(values)
     mycursor.execute(sql)
     result = mycursor.fetchall()
     return result
