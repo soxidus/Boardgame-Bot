fa15501e75f7657120c15f090164e42a3c2b8ba6
Christian R <s6chratt@uni-bonn.de>
2020-01-23 23:18:34 +0100

    use gamenight obect

--- src/calendar_export.py
+++ src/calendar_export.py
@@ -6,12 +6,13 @@ from parse_strings import generate_uuid_32
 
 def create_ics_file(title, date):
     calendar = Calendar()
+    gn = GameNight()
     gamenight_event = Event()
     gamenight_event.name = title
     gamenight_event.begin = date.datetime.isoformat()
     calendar.events.add(gamenight_event)
     filename = 'cal-' + generate_uuid_32() + '.ics'
-    GameNight(chat_id=update.callback_query.message.chat_id).set_cal_file(filename)
+    gn(chat_id=update.callback_query.message.chat_id).set_cal_file(filename)
     gamenight_event.make_all_day()
     with open(filename, 'w+') as my_file:
         my_file.writelines(c)
