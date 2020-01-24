9b68532a1baece5796b55658977990cb1abda7bd
Christian R <s6chratt@uni-bonn.de>
2020-01-25 00:04:13 +0100

    no need to double init

--- src/calendar_export.py
+++ src/calendar_export.py
@@ -1,19 +1,16 @@
 from ics import Calendar, Event
 from os import remove
-import datetime
-from planning_functions import GameNight
 from parse_strings import generate_uuid_32
 
 
 def create_ics_file(title, date):
     calendar = Calendar()
-    gn = GameNight()
     gamenight_event = Event()
     gamenight_event.name = title
     gamenight_event.begin = date.isoformat()
     calendar.events.add(gamenight_event)
     filename = 'cal-' + generate_uuid_32() + '.ics'
-    gn(chat_id=update.callback_query.message.chat_id).set_cal_file(filename)
+    GameNight(chat_id=update.callback_query.message.chat_id).set_cal_file(filename)
     gamenight_event.make_all_day()
     with open(filename, 'w+') as my_file:
         my_file.writelines(c)
