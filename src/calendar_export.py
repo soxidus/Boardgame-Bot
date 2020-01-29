6fc2d014573191cf6a7d6bd94b8b917ae0e1adce
Christian R <s6chratt@uni-bonn.de>
2020-01-29 21:07:46 +0100

    adjust calendar call

--- src/calendar_export.py
+++ src/calendar_export.py
@@ -1,4 +1,5 @@
-from ics import Calendar, Event
+from icalendar import Calendar, Event
+from datetime import datetime
 from os import remove
 
 from parse_strings import generate_uuid_32
@@ -6,15 +7,19 @@ from parse_strings import generate_uuid_32
 
 def create_ics_file(title, date):
     calendar = Calendar()
+    calendar.add('prodid', '-//Meeple - Auto-Ical//')
+    calendar.add('version', '2.0')
     gamenight_event = Event()
-    gamenight_event.name = title
-    gamenight_event.begin = date.isoformat()
-    calendar.events.add(gamenight_event)
+    gamenight_event.add('dtstamp', datetime.now())
+    gamenight_event.add('summary', title)
+    gamenight_event.add('description', 'Created by Meeple the boardgame bot')
+    gamenight_event.add('dtstart', datetime.strptime(date + " 19", "%d/%m/%Y %H"))
+    gamenight_event.add('dtend',  datetime.strptime(date + " 23", "%d/%m/%Y %H"))
+    calendar.add_component(gamenight_event)
     filename = 'cal-' + generate_uuid_32() + '.ics'
-#    GameNight(chat_id=update.callback_query.message.chat_id).set_cal_file(filename)
-    gamenight_event.make_all_day()
-    with open(filename, 'w+') as my_file:
-        my_file.writelines(calendar)
+
+    with open(filename, 'wb') as my_file:
+        my_file.write(Calendar.to_ical())
 
     return filename
 
