74f331e7efb47e555a090234af857815c23eb1ab
karacolmar <k.moraw@uni-bonn.de>
2020-07-01 21:29:10 +0200

    update error_handler dialogue

--- src/error_handler.py
+++ src/error_handler.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 
 from database_functions import check_notify
+from parse_strings import read_json
 
 
 def handle_bot_not_admin(bot, chat_id):
@@ -11,25 +12,13 @@ def handle_bot_not_admin(bot, chat_id):
     notify = check_notify("group_settings", chat_id, "notify_not_admin")
     if not bot_is_admin and notify:
         bot.send_message(chat_id,
-                         'Hey Leute, leider bin ich hier kein Admin. '
-                         'Das fällt gerade auf, weil ich so ein paar '
-                         'meiner Funktionen nicht nutzen kann:\n'
-                         '- Gruppennamen verändern '
-                         '(um das Datum des nächsten Spieleabends einzutragen)\n'
-                         '- Gruppenbeschreibung verändern '
-                         '(um eine Teilnehmerliste zu führen)\n'
-                         '- Nachrichten von anderen Gruppenmitgliedern löschen '
-                         '(ZENSUR! Nein, im Ernst, es macht alles übersichtlicher.)\n'
-                         'Bitte überlegt euch, ob ihr das wirklich verpassen wollt :)')
+                         read_json(["error_handler", "not_admin"]))
 
 
 def handle_bot_unauthorized(bot, chat_id, user, try_again=None):
-    text = ('OH! Scheinbar darf ich nicht privat mit dir reden, ' +
-            user + '. '
-            'Versuche, dich in einem Privatchat mit mir '
-            'mit /start oder /key zu authentifizieren.')
+    text = (read_json(["error_handler", "not_authorised"]).format(name=user))
     if try_again:
-        text += ' Danach kannst du ' + try_again + ' nochmal probieren.'
+        text += read_json(["error_handler", "try_again"]).format(try_what=try_again)
     notify = check_notify("group_settings", chat_id, "notify_unauthorized")
     if notify:
         bot.send_message(chat_id, text)
